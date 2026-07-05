6b7efdebb0a5820e9e7f44416a9df737de07591f
Agustina <agustinadgdl@gmail.com>
2026-07-05 20:31:00 -0300

    Bump service worker cache to v2 to force refresh

--- sw.js
+++ sw.js
@@ -1,2 +1,3 @@
-self.addEventListener('install', e => e.waitUntil(caches.open('cm-v1').then(c => c.addAll(['/','index.html','logo.png','manifest.json']))));
+self.addEventListener('install', e => e.waitUntil(caches.open('cm-v2').then(c => c.addAll(['/','index.html','logo.png','manifest.json']))));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!=='cm-v2').map(k=>caches.delete(k))))));
 self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
